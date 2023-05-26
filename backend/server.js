3ddc54bbe0ce379d94a040ad369a68abb30f02b5
Harshal141 <harshalmukundapatil@gmail.com>
2023-05-26 23:46:21 +0530

    temp pause

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import db from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
-import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 dotenv.config();
 
 // get port number from .env or use 5000 if not found
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB();
 
 app.use('/api/users', userRoutes);
 
